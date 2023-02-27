2b2e6ffc818df63280241f504b475ebf94b8f5e7
randomusernamea <stars.a@outlook.com>
2023-02-27 20:42:43 -0300

    Header Hotfix

--- Front/src/API/rule_crear.jsx
+++ Front/src/API/rule_crear.jsx
@@ -4,7 +4,7 @@ export const crearPokemon = (form) => {
   console.log(form)
   console.log(form.get('Imagen'))
   console.log(form.get('Pokemon'))
-  return API.post("/pokemon/nuevo", form)
+  return API.post("/pokemon/nuevo", form, {headers: { "Content-Type": "multipart/form-data" }} )
     .then((response) => {
       return response.data;
     })
